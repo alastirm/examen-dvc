835562e195ece0068dda0ba63180c3c3c5573918
alastirm <bruno.vermont@gmail.com>
2025-03-12 15:51:50 +0100

    test

--- src/data/split_data.py
+++ src/data/split_data.py
@@ -16,9 +16,11 @@ def main(input_filepath, output_filepath):
     logger = logging.getLogger(__name__)
     logger.info('making training data set from raw data')
 
-    input_filepath = click.prompt('Enter the file path for the input data', type=click.Path(exists=True))
+    #input_filepath = click.prompt('Enter the file path for the input data', type=click.Path(exists=True))
+    input_filepath = "../../data/raw_data"
     input_filepath_data = f"{input_filepath}/raw.csv"
-    output_filepath = click.prompt('Enter the file path for the output processed data (e.g., data/processed_data', type=click.Path())
+    #output_filepath = click.prompt('Enter the file path for the output processed data (e.g., data/processed_data', type=click.Path())
+    output_filepath = "../../data/processed_data"
     process_data(input_filepath_data, output_filepath)
 
 
